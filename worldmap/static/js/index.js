f1d15a1d34af4077804120241063aaf3c911096d
Niran814804102 <814804102@qq.com>
2017-11-28 17:16:02 +0800

    静态文件1

---
+++ worldmap/static/js/index.js
@@ -0,0 +1,38 @@
+// init for index.jsp
+
+$(document).ready(function (e) {
+	$('html').css(
+		'width',window.screen.availWidth-20);
+	$('#myCarousel').carousel({
+	interval: 2000
+	});
+	checkAuthority();
+	showMaps("tri","total");
+	showMaps("tmi1",1);
+	showMaps("tmi2",2);
+	showMaps("tmi3",3);
+});
+
+
+function showMaps(divIdPrefix,type){
+	var data=null;
+	$.ajax({
+		url: "./getMapListForIndex.action",
+		async: false,
+		type: "POST",
+		dataType: "text",
+		data: {type:type},
+		success: function (res) {
+			data = $.parseJSON(res);
+		}
+	});
+	for(maps in data){
+		var mapid = data[maps].id;
+		var mapname = data[maps].mapname;
+		var curdiv = divIdPrefix + (parseInt(maps)+1);
+		var img = $("#"+curdiv).find("img");
+		$("#"+curdiv).attr("onclick","location='./main.action?mapid="+mapid+"'");
+		$("#"+curdiv).children("p").text(mapname);
+		img.attr("src","./img/map/"+mapid+".jpg");
+	}	
+}
